9d4f029077c7846ca813eb806f5c24ddc970203a
LHV <lhv@LHVs-MacBook-Pro.local>
2024-06-11 16:47:30 +0700

    show movies on home page

--- spec/views/movies/new.html.erb_spec.rb
+++ spec/views/movies/new.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "movies/new", type: :view do
   before(:each) do
-    assign(:movie, Movie.new())
+    assign(:movie, Movie.new)
   end
 
   it "renders new movie form" do
